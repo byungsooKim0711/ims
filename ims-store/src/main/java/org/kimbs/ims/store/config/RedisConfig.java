65efad5e6a10cdff12038f240c3ecefc2c8de575
kimbs <vlakd0711@naver.com>
2021-09-02 23:36:37 +0900

    RedisConfig 변경

--- ims-store/src/main/java/org/kimbs/ims/store/config/RedisConfig.java
+++ ims-store/src/main/java/org/kimbs/ims/store/config/RedisConfig.java
@@ -1,38 +1,14 @@
 package org.kimbs.ims.store.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-//    @Value("${spring.redis.password}")
-//    private String redisPassword;
-
-    @Value("${spring.redis.database}")
-    private int redisDatabase;
-
-    @Primary
-    @Bean
-    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
-        factory.setDatabase(redisDatabase);
-
-        return factory;
-    }
 
     @Bean
     public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory cf) {
