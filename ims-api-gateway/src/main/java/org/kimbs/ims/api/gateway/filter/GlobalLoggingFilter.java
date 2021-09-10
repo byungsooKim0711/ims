835fd1dca6e7439cb4dfc9c16946f532761478fc
kimbs <vlakd0711@naver.com>
2021-09-10 23:03:24 +0900

    global filter에 elapsed time logging 추가

--- ims-api-gateway/src/main/java/org/kimbs/ims/api/gateway/filter/GlobalLoggingFilter.java
+++ ims-api-gateway/src/main/java/org/kimbs/ims/api/gateway/filter/GlobalLoggingFilter.java
@@ -6,18 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @Slf4j
 @Configuration
 public class GlobalLoggingFilter {
 
     @Bean
     public GlobalFilter postGlobalFilter() {
+        LocalDateTime startDate = LocalDateTime.now();
+
         return ((exchange, chain) -> chain.filter(exchange)
                 .then(Mono.fromRunnable(() -> {
                     String requestPath = exchange.getRequest().getPath().toString();
                     String requestId = exchange.getRequest().getId();
 
-                    log.info("Global logging filter. path: {}, id: {}", requestPath, requestId);
+                    log.info("Global logging filter. path: {}, id: {}, elapsedTime: {}", requestPath, requestId, Duration.between(startDate, LocalDateTime.now()));
                 }))
         );
     }
