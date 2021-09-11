3f217fb587286ef53bb386f4a35372d0870920ce
kimbs <vlakd0711@naver.com>
2021-09-12 00:46:23 +0900

    validation, onException -> AbstractClass 에 구현

--- ims-api-mt/src/main/java/org/kimbs/ims/mt/route/AbstractRouteHandler.java
+++ ims-api-mt/src/main/java/org/kimbs/ims/mt/route/AbstractRouteHandler.java
@@ -1,16 +1,23 @@
 package org.kimbs.ims.mt.route;
 
+import lombok.extern.slf4j.Slf4j;
+import org.kimbs.ims.exception.ImsBadRequestException;
+import org.kimbs.ims.exception.ImsTooLongMessageException;
+import org.kimbs.ims.exception.NotSupportMessageType;
 import org.kimbs.ims.mt.config.ApiMtConfig;
 import org.kimbs.ims.mt.service.KafkaService;
 import org.kimbs.ims.protocol.ImsApiResult;
 import org.kimbs.ims.protocol.ImsPacket;
 import org.kimbs.ims.protocol.code.ResponseCode;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.KafkaException;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Validator;
 
+@Slf4j
 public abstract class AbstractRouteHandler<R, M> {
 
     protected final Validator validator;
@@ -40,14 +47,37 @@ public abstract class AbstractRouteHandler<R, M> {
 
     protected abstract ImsPacket<M> convert(R request);
 
-    protected abstract void validation(R request);
+    protected void validation(R request) {
+        validator.validate(request)
+                .stream()
+                .findAny()
+                .ifPresent(v -> {
+                    throw new ImsBadRequestException(v.getMessage());
+                });
+    }
 
     protected abstract void checkMandatory(ImsPacket<M> message);
     protected abstract void checkDuplicateMsgUid(ImsPacket<M> message);
-
     protected abstract void send(ImsPacket<M> message);
-
-    protected abstract Mono<ServerResponse> onException(Throwable e);
-
     protected abstract void log(ImsPacket<M> message);
+
+    protected  Mono<ServerResponse> onException(Throwable e) {
+        // TODO: exception logging 필요함. request 데이터를 어떻게 가져올 수 있을까...
+
+        if (e instanceof ImsBadRequestException) {
+            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST, e.getMessage()));
+        } else if (e instanceof NotSupportMessageType) {
+            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST, e.getMessage()));
+        } else if (e instanceof ServerWebInputException) {
+            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST));
+        } else if (e instanceof ImsTooLongMessageException) {
+            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.TOO_LONG_MESSAGE_EXCEPTION, e.getMessage()));
+        } else if (e instanceof KafkaException) {
+            log.error("kafka exception.", e);
+            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.SERVICE_SERVER_ERROR));
+        }
+
+        log.error("unexpected exception occurred.", e);
+        return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.SERVICE_SERVER_ETC_ERROR));
+    }
 }
--- ims-api-mt/src/main/java/org/kimbs/ims/mt/route/MtRouteHandler.java
+++ ims-api-mt/src/main/java/org/kimbs/ims/mt/route/MtRouteHandler.java
@@ -3,26 +3,19 @@ package org.kimbs.ims.mt.route;
 import lombok.extern.slf4j.Slf4j;
 import org.kimbs.ims.exception.ImsBadRequestException;
 import org.kimbs.ims.exception.ImsTooLongMessageException;
-import org.kimbs.ims.exception.NotSupportMessageType;
 import org.kimbs.ims.model.mt.MtMessageReq;
 import org.kimbs.ims.model.mt.MtMessageType;
 import org.kimbs.ims.mt.config.ApiMtConfig;
 import org.kimbs.ims.mt.service.KafkaService;
-import org.kimbs.ims.protocol.ImsApiResult;
 import org.kimbs.ims.protocol.ImsPacket;
 import org.kimbs.ims.protocol.ImsPacketCommand;
-import org.kimbs.ims.protocol.code.ResponseCode;
 import org.kimbs.ims.protocol.v1.mt.ImsMtReq;
 import org.kimbs.ims.protocol.v1.trace.TraceInfo;
 import org.kimbs.ims.util.ByteUtil;
 import org.kimbs.ims.util.RoundRobinUtil;
 import org.kimbs.ims.util.SerialNumberUtil;
-import org.springframework.kafka.KafkaException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import org.springframework.web.server.ServerWebInputException;
-import reactor.core.publisher.Mono;
 
 import javax.validation.Validator;
 import java.time.LocalDateTime;
@@ -61,17 +54,6 @@ public class MtRouteHandler extends AbstractRouteHandler<ImsMtReq, MtMessageReq>
                 .data(mtMessageReq)
                 .traceInfo(traceInfo)
                 .build();
-    }
-
-    @Override
-    protected void validation(ImsMtReq request) {
-        validator.validate(request)
-                .stream()
-                .findAny()
-                .ifPresent(v -> {
-                    log.info("[request error] message_id: {}, cause: {}", request.getMessageId(), v.getMessage());
-                    throw new ImsBadRequestException(v.getMessage());
-                });
     }
 
     @Override
@@ -114,27 +96,6 @@ public class MtRouteHandler extends AbstractRouteHandler<ImsMtReq, MtMessageReq>
         List<String> mtTopics = config.getTopics().getRecvMt();
 
         kafkaService.sendToKafka(RoundRobinUtil.getRoundRobinValue(RoundRobinUtil.RoundRobinKey.RECV_MT, mtTopics), message);
-    }
-
-    @Override
-    protected Mono<ServerResponse> onException(Throwable e) {
-        // TODO: exception logging 필요함. request 데이터를 어떻게 가져올 수 있을까...
-
-        if (e instanceof ImsBadRequestException) {
-            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST, e.getMessage()));
-        } else if (e instanceof NotSupportMessageType) {
-            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST, e.getMessage()));
-        } else if (e instanceof ServerWebInputException) {
-            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.BAD_REQUEST));
-        } else if (e instanceof ImsTooLongMessageException) {
-            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.TOO_LONG_MESSAGE_EXCEPTION, e.getMessage()));
-        } else if (e instanceof KafkaException) {
-            log.error("kafka exception.", e);
-            return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.SERVICE_SERVER_ERROR));
-        }
-
-        log.error("unexpected exception occurred.", e);
-        return ServerResponse.ok().bodyValue(ImsApiResult.failed(ResponseCode.SERVICE_SERVER_ETC_ERROR));
     }
 
     @Override
